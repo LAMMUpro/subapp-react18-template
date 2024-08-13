161fb58baf504e8aefd026906a01b6a5fdcbc30c
陈永超 <chen_yongchao@126.com>
2024-08-13 16:18:16 +0800

    派发组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import { useState } from 'react';
+import MicroComponent from 'micro-app-utils/react18/MicroComponent';
+
+function Footer(props: any) {
+  return <div>
+    <div>uuid: {props.uuid}</div>
+    <div>id: {props.id}</div>
+  </div>
+}
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [showDialog, setShowDialog] = useState(false);
   return (
     <>
       <div>
-        <div>react 应用: {count}</div>
-        <button onClick={() => setCount(count + 1)}>计数器+1</button>
+        <button onClick={() => setShowDialog(!showDialog)}>显示弹窗</button>
+
+        <MicroComponent
+          _is="BaseDialog"
+          modelValue={showDialog}
+          onUpdate:modelValue={() => setShowDialog(false)}
+          default={<div>默认</div>}
+          footer={<Footer id='123'>脚部</Footer>}
+        >
+        </MicroComponent>
       </div>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
 import { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { generateDataListener } from 'micro-app-utils/listener';
+import { MicroComponentSlotMap } from 'micro-app-utils/data';
+import React from 'react';
 
 let app = createRoot(document.getElementById('root')!);
+
+/** 监听微前端主应用数据 */
+let dataListener: (data: BaseObj<any>) => void;
 
 window.mount = () => {
   app.render(
@@ -10,9 +17,23 @@ window.mount = () => {
       <App />
     </StrictMode>
   );
+  dataListener = generateDataListener({
+    micro_component: ({ slotName, elementId, props, parentElementId }) => {
+      const Element = document.body.querySelector(`#${elementId}`);
+      const component = MicroComponentSlotMap[parentElementId]?.[slotName];
+      if (Element && component) {
+        createRoot(Element).render(React.createElement(component.type, {
+          ...component.props,
+          ...props,
+        }));
+      }
+    },
+  });
+  window.microApp?.addDataListener(dataListener, true);
 }
 
 window.unmount = () => {
+  window.microApp?.removeDataListener(dataListener);
   app.render(<div></div>);
 }
 
