ca96436af1f6e458428952d4faa2eaa3505b9963
王伟杰 <wangweijieyanfa@ctv.org.cn>
2024-08-29 14:53:13 +0800

    feat: 删除无效代码片段

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,10 @@
-import { useEffect } from 'react';
-import {
-  RouterProvider,
-  useLocation,
-  useNavigate
-} from 'react-router-dom';
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+
 import router from '@/router';
 
-const NavigatorFromBaseApp = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
-  useEffect(() => {
-    window.microApp?.addDataListener(data => {
-      // TODO
-      // 当基座下发path时进行跳转
-      if (data.path && data.path !== history.location.pathname) {
-        navigate.push(data.path as string);
-      }
-    });
-  }, [location]);
-};
-
 export default function App() {
-  return <>
-
-    <RouterProvider router={router} fallbackElement={<p>loading</p>} >
-      <NavigatorFromBaseApp/>
-    </RouterProvider>
-  </>;
+  return <RouterProvider router={router} fallbackElement={<p>loading</p>} />;
 }
 
 if (import.meta.hot) {
