b140da52543f332cbd4b1b737c5cd00390462528
王伟杰 <wangweijieyanfa@ctv.org.cn>
2024-08-30 18:33:19 +0800

    feat: 点击事件数数据不更新问题处理

--- src/pages/demo/micromainComponent.tsx
+++ src/pages/demo/micromainComponent.tsx
@@ -1,9 +1,29 @@
-import MicroComponent from 'micro-app-utils/react18/MicroComponent';
 import { useState } from 'react';
+
+import MicroComponent from 'micro-app-utils/react18/MicroComponent';
 
 function micromainComponent() {
   const [isShowDialog, setIsShowDialog] = useState(false);
   const [number, setNumber] = useState(1);
+
+  const defaultDOM = (
+    <div>
+      <div>默认插槽:{number}</div>
+      <button
+        style={{
+          backgroundColor: 'lightskyblue',
+          color: 'white',
+          fontSize: '14px',
+          borderRadius: '4px',
+          padding: '6px 10px',
+        }}
+        onClick={() => setNumber((val) => val + 1)}
+      >
+        计数器加1
+      </button>
+    </div>
+  );
+
   return (
     <>
       <div>
@@ -36,23 +56,7 @@ function micromainComponent() {
           <MicroComponent
             _is="BaseDialog"
             modelValue={isShowDialog}
-            default={
-              <div>
-                <div>默认插槽:{number}</div>
-                <button
-                  style={{
-                    backgroundColor: 'lightskyblue',
-                    color: 'white',
-                    fontSize: '14px',
-                    borderRadius: '4px',
-                    padding: '6px 10px',
-                  }}
-                  onClick={() => setNumber(number + 1)}
-                >
-                  计数器加1
-                </button>
-              </div>
-            }
+            default={defaultDOM}
             footer={<div>脚部</div>}
           ></MicroComponent>
         </section>
