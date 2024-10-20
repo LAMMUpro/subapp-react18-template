e6f0409ee2834efc3c48548a69cd7e0751aa34d6
LAMMU <chen_yongchao@126.com>
2024-10-20 10:31:53 +0800

    factor: 微前端环境延迟初始化 解决第三层应用无法使用MicroApp的限制

--- src/main.tsx
+++ src/main.tsx
@@ -5,57 +5,66 @@ import microApp from '@micro-zoe/micro-app';
 
 import { isTopApp, sendDataDown, sendDataUp, MicroAppInit } from 'micro-app-tools';
 import { generateDataListener } from 'micro-app-tools/listener';
-import { MicroComponentSlotMap, ReactMicroComponentSlotInfoMap } from 'micro-app-tools/data';
+import { MicroAppConfig, MicroComponentSlotMap, ReactMicroComponentSlotInfoMap, setMicroAppInitFunction } from 'micro-app-tools/data';
 
 import { routes } from '@/router';
 import CONSTS from '@/utils/CONSTS';
 
 import App from './App.tsx';
 
+/** 从父应用获取子应用配置 */
 window._subAppSettingList_ = window.rawWindow?._subAppSettingList_ || [];
+/** 虽然微前端环境初始化延迟了, 但这个配置还是要提前传过去 */
+MicroAppConfig.subAppSettingList = window._subAppSettingList_;
 
-/** 初始化微前端配置 */
-MicroAppInit<'localhost' | 'test' | 'pre' | 'master'>({
-  env: process.env.NODE_ENV === 'development' ? 'localhost' : 'master',
-  tagName: CONSTS.microAppTagName,
-  dataListener: generateDataListener({
-    /** 子应用接收到这个请求需要往上传递，直到传给顶部主应用 */
-    micro_component_request: (data) => {
-      sendDataUp({
-        emitName: 'micro_component_request',
-        parameters: [{
-          ...data,
-          subAppNameList: [...data.subAppNameList, window.__MICRO_APP_NAME__!]
-        }],
-      });
-    },
-    /** 子应用接收到这个请求需要往上传递，直到传给顶部主应用 */
-    micro_component_destroy: (elementId) => {
-      sendDataUp({
-        emitName: 'micro_component_destroy',
-        parameters: [elementId],
-      });
-    },
-    /** 子应用接收到这个请求需要往上传递，直到传给顶部主应用 */
-    micro_component_clear_props_slots: (elementId: string) => {
-      sendDataUp({
-        emitName: 'micro_component_clear_props_slots',
-        parameters: [elementId],
-      });
-    },
-  }),
-  subAppSettingList: window._subAppSettingList_,
-});
-
-/** 
- * 初始化子应用渲染环境（默认主应用/第一层子应用执行）
+/**
+ * 微前端环境并非一开始就初始化, 要等使用MicroApp时才决定是否初始化
  */
-if (window._subAppSettingList_.find(item => item.name === window.__MICRO_APP_NAME__)) microApp.start({
-  tagName: CONSTS.microAppTagName,
-  /** 防止子应用请求父应用资源（部署时需要配置这个url指向这个文件） */
-  iframeSrc: `/micromain/empty.html`,
-  'keep-router-state': true,
-});
+setMicroAppInitFunction(() => {
+  /** 初始化微前端配置 */
+  MicroAppInit<'localhost' | 'test' | 'pre' | 'master'>({
+    env: process.env.NODE_ENV === 'development' ? 'localhost' : 'master',
+    tagName: CONSTS.microAppTagName,
+    dataListener: generateDataListener({
+      /** 子应用接收到这个请求需要往上传递，直到传给顶部主应用 */
+      micro_component_request: (data) => {
+        sendDataUp({
+          emitName: 'micro_component_request',
+          parameters: [{
+            ...data,
+            subAppNameList: [...data.subAppNameList, window.__MICRO_APP_NAME__!]
+          }],
+        });
+      },
+      /** 子应用接收到这个请求需要往上传递，直到传给顶部主应用 */
+      micro_component_destroy: (elementId) => {
+        sendDataUp({
+          emitName: 'micro_component_destroy',
+          parameters: [elementId],
+        });
+      },
+      /** 子应用接收到这个请求需要往上传递，直到传给顶部主应用 */
+      micro_component_clear_props_slots: (elementId: string) => {
+        sendDataUp({
+          emitName: 'micro_component_clear_props_slots',
+          parameters: [elementId],
+        });
+      },
+    }),
+    subAppSettingList: window._subAppSettingList_ || [],
+  });
+
+  /** 
+   * 初始化子应用渲染环境（默认主应用/第一层子应用执行）
+   */
+  microApp.start({
+    tagName: CONSTS.microAppTagName,
+    /** 防止子应用请求父应用资源（部署时需要配置这个url指向这个文件） */
+    iframeSrc: `/micromain/empty.html`,
+    'keep-router-state': true,
+  });
+})
+
 
 let app = createRoot(document.getElementById('__subapp_react18')!);
 
